335ff80aa04fb11effeb3cebde11737c5805cd17
shaeera <shaeeraa1@gmail.com>
2021-03-07 16:12:53 +0000

    task A completed

--- PIH.py
+++ PIH.py
@@ -4,10 +4,29 @@ class Person:
         self._gender = gender.upper()
 
     def __str__(self):
-        return "Name: " + self._name+ " Gender: " + self._gender
+        return "Name: " + self._name + "\nGender: " + self._gender
+
+class Employee(Person):
+    def __init__(self, name, gender, work_department):
+        super().__init__(name, gender)
+        self._work_department = work_department
+
+    def __str__(self):
+        return "Name: " + self._name + "\nGender: " + self._gender + "\nDepartment: " + self._work_department
 
 
 
-    # def display(self):
-    #     print('Name:', self.name)
-    #     print('Age:', self.age)
+class Customer(Person):
+    def __init__(self, name, gender, shop_department):
+        super().__init__(name, gender)
+        self._shop_department = shop_department
+
+    def __str__(self):
+        return "Name: " + self._name + "\nGender: " + self._gender + "\nDepartment: " + self._shop_department
+
+
+
+        # def display(self):
+        #     print('Name:', self.name)
+        #     print('Age:', self.age)
+
--- inheritence.py
+++ inheritence.py
@@ -1,6 +1,15 @@
 from PIH import Person
+from PIH import Employee
+from PIH import Customer
 
-manz_person = Person('manz', 'F')
+manz_person = Person('Manz', 'F')
 print(manz_person)
 
+manz_employee = Employee('Manz', 'F', 'Gaming')
+print(manz_employee)
 
+shaeera_customer = Customer('Shaeera', 'F', 'Gaming')
+print(shaeera_customer)
+
+if manz_employee._work_department == shaeera_customer._shop_department:
+    print("Shopping")
