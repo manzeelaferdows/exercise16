60e94be12d68b40dcb68ea931832845f09c797b4
shaeera <shaeeraa1@gmail.com>
2021-03-08 19:08:43 +0000

    almost complete task b

--- bank_account.py
+++ bank_account.py
@@ -1,11 +1,21 @@
 class Account:
-  # numCreated = 0
-  def __init__(self, initial_balance):
-      # self._account_type = account_type
-      self._initial_balance = initial_balance
-      # numCreated += 1
-  def withdrawn(self, amount):
-      self._balance -= amount
-      return
+    # numCreated = 0
+    def __init__(self, initial_balance):
+        # self._account_type = account_type
+        self._balance = initial_balance
+        # numCreated += 1
 
+    def withdrawn(self, amount):
+        self._balance -= amount
+        return self._balance
 
+class Saving_Account(Account):
+    def __init__(self, initial_balance):
+        super().__init__(initial_balance)
+
+    def deposit(self, amount):
+        self._balance += amount
+        return self._balance
+
+    # def getbalance(self):
+    #     return self._balance
--- bank_process.py
+++ bank_process.py
@@ -1,8 +1,11 @@
-from bank_account_types import Account
-from saving_account import Saving_Account
+from bank_account import Account
+from bank_account import Saving_Account
 
 joint_account = Account(1000)
-savings_account = Saving_Account()
+savings_account = Saving_Account(50)
+
+print(joint_account._withdrawn(10))
+
 
 print(joint_account)
 print(savings_account)
